2bf5b2d813048807cb08a8a8b50a44ece3ea73aa
mali864 <mali86468744@gmail.com>
2025-03-07 14:41:28 +0500

    code
    push code new

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import Navbar from './component/Navbar';
+import Home from './pages/Home';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import About from './pages/About';
+import Services from './pages/Services';
+import Sidebar from './component/Sidebar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
+  const showSidebar = location.pathname !== '/' && location.pathname !== "/forgot";
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className="flex">
+        {/* Conditionally render Sidebar */}
+        {showSidebar && <Sidebar />}
+
+
+        {/* Main Content Area Section */}
+        <div className="flex-1 p-5">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
